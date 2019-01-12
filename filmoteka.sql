35fa97dc8c9fba25fcb976c1ee3405e7bdf8c50a
Ksenia <ksenia.lavruk@yandex.ru>
2019-01-12 15:48:10 +0300

    file sql

---
+++ filmoteka.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Янв 12 2019 г., 15:46
+-- Версия сервера: 5.6.41
+-- Версия PHP: 5.5.38
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `filmoteka`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `films`
+--
+
+CREATE TABLE `films` (
+  `id` int(11) NOT NULL,
+  `title` text NOT NULL,
+  `genre` text NOT NULL,
+  `year` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `films`
+--
+
+INSERT INTO `films` (`id`, `title`, `genre`, `year`) VALUES
+(1, 'Облачный атлас', 'Драма', 2012),
+(2, 'Такси 2', 'Комедия', 2000),
+(3, 'Отражение', 'Триллер', 2008),
+(4, 'Праздничный переполох', 'Драма', 2017);
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `films`
+--
+ALTER TABLE `films`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `films`
+--
+ALTER TABLE `films`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
