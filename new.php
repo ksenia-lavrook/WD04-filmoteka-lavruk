731bd04c70e9ad1117f3a34dad7cd5eb455d89fc
Ksenia <ksenia.lavruk@yandex.ru>
2019-01-17 16:46:39 +0300

    adding description and photo

--- new.php
+++ new.php
@@ -6,8 +6,7 @@ $link = db_connect();
 
 require('models/films.php');
 
-// UPDATE film data in DB
-if ( array_key_exists('update-film', $_POST) ) {
+if ( array_key_exists('add-film', $_POST) ) {
 	
 	// Обработка ошибок
 	if ( $_POST['title'] == '') {
@@ -20,22 +19,21 @@ if ( array_key_exists('update-film', $_POST) ) {
 		$errors[] = "<p>Необходимо ввести год фильма!</p>";
 	}
 
+	// Если ошибок нет - сохраняем фильм
 	if ( empty($errors) ) {
-
-		$result = film_update($link, $_POST['title'], $_POST['genre'], $_POST['year'], $_GET['id'], $_POST['description']);
+		$result = film_new($link, $_POST['title'], $_POST['genre'], $_POST['year'], $_POST['description']);
 		if ( $result ) {
-			$resultSuccess = "<p>Фильм был успешно обновлен!</p>";
+			$resultSuccess = "<p>Фильм был успешно добавлен!</p>";
 		} else { 
 			$resultError = "<p>Что то пошло не так. Добавьте фильм еще раз!</p>";
 		}
 	}
 }
 
-$film = get_film($link, $_GET['id']);
-
 include('views/head.tpl');
 include('views/notifications.tpl');
-include('views/edit-film.tpl');
+include('views/new-film.tpl');
 include('views/footer.tpl');
 
 ?>
+
--- single.php
+++ single.php
@@ -17,11 +17,15 @@ if ( @$_GET['action'] == 'delete') {
 	}
 }
 
-$films = films_all($link);
+$film = get_film($link, $_GET['id']);
+
+// echo "<pre>";
+// print_r($film);
+// echo "</pre>";
 
 include('views/head.tpl');
 include('views/notifications.tpl');
-include('views/index.tpl');
+include('views/film-single.tpl');
 include('views/footer.tpl');
 
 ?>
